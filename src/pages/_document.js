a72a28c0aff6ff7a5c5b6486ef44b3864cff9ad9
abhimanyu <iamabhimanyu555@gmail.com>
2023-08-25 10:49:44 +0530

    remove seo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,18 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-     <head>
-
-  
-      
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="description" content="Your digital marketing agency's compelling description goes here."/>
-    <meta name="keywords" content="digital marketing, SEO, social media, online advertising, branding"/>
-    <meta name="author" content="Your Name"/>
-    <title>Digital Marketing Services | Your Digital Agency Name</title>
-    
-     </head>
+     <Head />
       <body>
         <Main />
         <NextScript />
