3990b19b69f411b357956e257a5a2f648718de00
abhimanyu <iamabhimanyu555@gmail.com>
2023-10-02 21:41:39 +0530

    pixel added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,31 +1,63 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-     <Head>
-     <Script
-  strategy="lazyOnload"
-  src={`https://www.googletagmanager.com/gtag/js?id=G-SC6GS5G8C5`}
-/>
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* <!-- Meta Pixel Code --> */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1024110142264599');
+              fbq('track', 'PageView');
+              `,
+            }}
+          ></script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=1024110142264599&ev=PageView&noscript=1"
+            />
+          </noscript>
+          {/* <!-- End Meta Pixel Code --> */}
 
-<Script id="ga-script" strategy="lazyOnload">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-SC6GS5G8C5', {
-      page_path: window.location.pathname,
-    });
-        `}
-</Script>
-      
-      
-         </Head> 
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+          {/* Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-SC6GS5G8C5`}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SC6GS5G8C5', {
+                page_path: window.location.pathname,
+              });
+              `,
+            }}
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
+ 
