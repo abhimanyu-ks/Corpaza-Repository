92cb01ab932d7a116f61cb6a24db4ffde326c5d8
abhimanyu <iamabhimanyu555@gmail.com>
2023-12-12 12:44:16 +0530

    all done

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -6,7 +6,7 @@ export default function SocialMedia() {
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
             <h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-900 ">Marketing Packages</h2>
-            <p class="mb-5  text-gray-800 text-lg ">Designed for business like yours</p>
+            <p class="mb-5  text-gray-800 text-lg ">Designed for business like yours..</p>
         </div>
         <div class="space-y-8 lg:grid lg:grid-cols-2 sm:gap-6 xl:gap-10 lg:space-y-0">
           
